9927299d723bcbb2a6d6a0b735f0dc4b4ff20a91
Vembarasan C <151906529+vembarasan-c@users.noreply.github.com>
2024-06-10 20:46:53 +0530

    Update projects.js

--- src/components/util/projects.js
+++ src/components/util/projects.js
@@ -15,6 +15,25 @@ const projects = [
     image: '/assets/images/project-3.jpg',
     technologiesUsed: ' React / Redux ',
   },
+  {
+    id: 'p3',
+    projectName: 'Movie Ticket Booking System',
+    link: ''https://github.com/vembarasan-c/movie-ticket-booking-backend,
+    image: '/assets/images/movie.jpg',
+    technologiesUsed: 'React / Spring boot and Mysql ',
+  },{
+    id: 'p4',
+    projectName: 'Social media app,
+    link: 'https://github.com/vembarasan-c/social-media-app-backend,
+    image: '/assets/images/media-1.jpg',
+    technologiesUsed: 'React / Spring boot and Mysql',
+  },{
+    id: 'p5',
+    projectName: 'Employee Management System',
+    link: 'https://github.com/vembarasan-c/employee-management-system',
+    image: '/assets/images/emp-1.jpg',
+    technologiesUsed: 'Thymeleaf and Springboot',
+  },
 ];
 
 export default projects;
