688a064b4c57075fe191e8ad3f43343c86211f5c
Vembarasan C <151906529+vembarasan-c@users.noreply.github.com>
2024-06-10 22:23:18 +0530

    Update Container.jsx

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -1,26 +1,25 @@
 function Containers({ name, language, link, image }) {
   return (
-    <section className="space-y-2 mt-5 mb-2">
+        <section className="space-y-2 mt-5 mb-2">
       <div className=" flex  justify-center">
-        <div className=" w-[320px] lg:w-[340px] rounded-xl lg:mx-80  bg-slate-800 py-2 px-2 ">
-          <a href={link}>
+        <div className=" w-[320px] shadow-md shadow-cyan-200 lg:w-[340px] rounded-xl lg:mx-80  bg-slate-800 py-2 px-2 ">
+          <a href={link} > 
             <img
-              className="border-2 rounded-sm object-cover w-full h-60  border-slate-700   "
+              className=" shadow-md  rounded-sm object-contain rounded-md w-full   border-slate-700   "
               src={image}
               alt="image"
             />
           </a>
-          <div className=" text-slate-200 text-center text-lg space-y-2 mt-2 ">
-            <p>{name}</p>
-            <p className="text-sm">{language}</p>
+
+          <div className=" text-slate-200  text-center  space-y-1 mt-2 ">
+            <hr className=" shadow-sm shadow-gray-300 border-gray-400 " />
+            <p className="text-lg font-semibold" >{name}</p>
+            <p className="text-gray-400 text-xs">{language}</p>
           </div>
         </div>
       </div>
     </section>
-  );
+
+    );
 }
 export default Containers;
-
-// i want to become a successful software developer
-// java is my favourite language
-// logical thinking criticalthinking problem solving, communication
