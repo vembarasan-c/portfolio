9a3c085b264642b4b60176c161a00619adcaf923
Vembarasan C <151906529+vembarasan-c@users.noreply.github.com>
2024-06-11 18:49:09 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import ConnectWithMe from './Connect';
 function Footer() {
   return (
     <footer>
-      <div className=" flex justify-center">
+      <div className=" mt-4 flex justify-center">
         <div className=" text-slate-400 space-y-3">
           <p>
             <span className="">&copy;</span> Vembarasan c | All rights reserved.
